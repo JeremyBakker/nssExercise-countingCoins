5383bc31cdc72aa648bd153a574313103a75dbf4
Jeremy Bakker <jeremybakker@Jeremys-MacBook-Pro.local>
2017-01-09 12:41:40 -0600

    amended instructions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nssExercise-countingCoins
+
+***Instructions***
+Write a program that will convert a dollar amount to the number of coins that make up the amount. Use the common American coins of quarters, dimes, nickels, and pennies.
--- coins.js
+++ coins.js
@@ -1,6 +1,3 @@
-/* Instructions
-Write a program that will convert a dollar amount to the number of coins that make up the amount. Use the common American coins of quarters, dimes, nickels, and pennies.*/
-
 //Initialize a coin counting function
 function coinCounter (amount) {
   
